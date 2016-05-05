81de8f38c51ec82f5e6c2192f07f2b8f844a0ebf
Trevor Edwards <tre23@cornell.edu>
2016-05-05 02:04:25 -0400

    remove debug log

--- routes/api/api.js
+++ routes/api/api.js
@@ -14,7 +14,6 @@ router.get('/colleges', function (req, res, next) {
 });
 
 router.get('/hardware', function (req, res, next) {
-    console.log('woo');
     Hardware.getAll(function (err, data) {
         if (err) console.log(err);
         else res.send(data);
