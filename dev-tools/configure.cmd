1c665923cdd622ce95c1235cccc05e534f5f2da1
Matthew Hsu <gears88@gmail.com>
2016-06-27 19:56:55 -0700

    update readme

--- dev-tools/configure.cmd
+++ dev-tools/configure.cmd
@@ -1,1 +1,8 @@
-cp git/pre-push ../.git/hooks/pre-push
+if exist git/pre-push (
+    cp git/pre-push ../.git/hooks/pre-push
+	echo "Git hooks successfully configured!"
+) else (
+    echo "ERROR: SCRIPT MUST BE RUN FROM /dev-tools directory!"
+)
+
+
--- dev-tools/configure.sh
+++ dev-tools/configure.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-cp git/pre-push ../.git/hooks/pre-push
+if [ -a ./git/pre-push ]
+then
+	cp ./git/pre-push ../.git/hooks/pre-push
+	echo "Git hooks successfully configured!"
+else
+	echo "ERROR: SCRIPT MUST BE RUN FROM /dev-tools directory!"
+fi
+
