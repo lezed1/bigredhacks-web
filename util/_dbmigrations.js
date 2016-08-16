8c5ffb6419f26942d662ccb528047b70c63941ca
Trevor Edwards <tre23@cornell.edu>
2016-08-15 23:37:11 -0400

    remove db migrations

--- util/_dbmigrations.js
+++ util/_dbmigrations.js
@@ -2,26 +2,5 @@ var async = require("async");
 var User = require("../models/user");
 var migration = {};
 
-/**
- * Back fill missing internal.daysToRSVP
- * @year 2016
- */
-migration.backFillRSVPTime = function() {
-    User.find( {'internal.daysToRSVP': {$exists: false}}).exec(function (err, users) {
-        const RATE_LIMIT = 3;
-        async.eachLimit(users, RATE_LIMIT, function(user,done) {
-            console.log('Migrating user: ' + user.name.full);
-            user.internal.daysToRSVP = 10; // Since this is run once, just using 10
-            user.save(done);
-        }, function(err) {
-            if (err) {
-                console.error(err);
-            }
-            else {
-                console.log("Migration completed.")
-            }
-        })
-    })
-};
 
 module.exports = migration;
