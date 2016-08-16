2d248c71de6aba9219b2b15fe233dceef6bb9c55
Juliana Hong <julianahhong@gmail.com>
2016-08-16 19:19:51 -0400

    Start working on sending email when click checkout hardware button

---
+++ public/js/hardware.js
@@ -0,0 +1,11 @@
+//send email when click checkout hardware button
+$("#checkout-hardware-btn").on('click', function (){
+    var name = $('#name').val();
+    var email = $('#email').val();
+    if (name == '' || email == '') {
+        return false;
+    }
+    else {
+        //to-do: email stuff here
+    }
+});
