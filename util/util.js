004b20af7bab5ebba3a82365109e2d4a37dd585a
Trevor Edwards <tre23@cornell.edu>
2016-08-15 21:12:28 -0400

    calendar 3 minute cache

--- util/util.js
+++ util/util.js
@@ -70,29 +70,47 @@ util.removeUserFromBus = function (Bus, req, res,user) {
     });
 };
 
-// Returns a sorted calendar (see APIdoc in api.js)
+// 3 minute cache
+const CACHE_EXPIRATION_IN_MILLIS = 1000 * 60 * 3;
+// Last grabbed calendar
+var cachedCalendar = null;
+// Date of when calendar was last updated
+var lastCalendarUpdate = null;
+/**
+ * Returns a sorted calendar (see APIdoc in api.js). Uses a simple cache
+ * to reduce load on ical source.
+ */
 util.grabCalendar = function grabCalendar(callback) {
-    request(config.setup.ical, function(err, response, ical) {
-        if (err) {
-            callback(err);
-        } else if (response.statusCode != 200) {
-            callback('ERROR: Bad response on calendar request!');
-        } else {
-            var calendar = icalendar.parse_calendar(ical);
+    if (!lastCalendarUpdate || lastCalendarUpdate < Date.now() - CACHE_EXPIRATION_IN_MILLIS) {
+        // This serves as a lock so that we do not repeat requests. While this occurs, stale calendar data will
+        // be fed temporarily to subsequent requests.
+        lastCalendarUpdate = Date.now();
+        request(config.setup.ical, function(err, response, ical) {
+            if (err) {
+                callback(err);
+            } else if (response.statusCode != 200) {
+                callback('ERROR: Bad response on calendar request!');
+            } else {
+                var calendar = icalendar.parse_calendar(ical);
 
-            let calendarEvents = calendar.events().map(element => {
-                return {
-                    event: element.properties.SUMMARY[0].value,
-                    start: element.properties.DTSTART[0].value,
-                    end: element.properties.DTEND[0].value,
-                    location: element.properties.LOCATION[0].value,
-                    description: element.properties.DESCRIPTION[0].value
-                }
-            }).sort( (x,y) => (x.start > y.start));
+                let calendarEvents = calendar.events().map(element => {
+                    return {
+                        event: element.properties.SUMMARY[0].value,
+                        start: element.properties.DTSTART[0].value,
+                        end: element.properties.DTEND[0].value,
+                        location: element.properties.LOCATION[0].value,
+                        description: element.properties.DESCRIPTION[0].value
+                    }
+                }).sort( (x,y) => (x.start > y.start));
 
-            callback(null, calendarEvents);
-        }
-    });
+                // Update cache
+                cachedCalendar = calendarEvents;
+                callback(null, calendarEvents);
+            }
+        });
+    } else {
+        callback(null, cachedCalendar);
+    }
 };
 
 module.exports = util;
