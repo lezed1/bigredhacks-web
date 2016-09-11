8637352497ab68cce3584e5799019c8e29fad5e8
Trevor Edwards <tre23@cornell.edu>
2016-09-11 18:22:22 -0400

    initial mentor request route

--- models/mentor_request.js
+++ models/mentor_request.js
@@ -1,14 +1,24 @@
 "use strict";
 var mongoose = require('mongoose');
-var User = require('./user');
 var en = require("./enum.js");
 
 var mentorRequestSchema = new mongoose.Schema({
+    description: {type: String, required: true}, //description of problem/reason for mentorship request
     user: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
     mentor: {type: mongoose.Schema.Types.ObjectId, ref: "Mentor", default: null},
-    description: {type: String, required: true}, //description of problem/reason for mentorship request
     status: {type: String, enum: en.mentorrequest.status, default: "Unclaimed"},
-    location: {type: String, default: "Unknown"} // Location of user who made the request. Usually a table number.
+    location: {type: String, default: "Unknown"}, // Location of user who made the request. Usually a table number.
+    createdAt: {type: Date, default: Date.now()}
 });
+
+mentorRequestSchema.statics.generateRequest = function(userId, descrip, loc, callback) {
+    var request = new this({
+        user: userId,
+        description: descrip,
+        location: loc
+    });
+
+    request.save(callback);
+};
 
 module.exports = mongoose.model("MentorRequest", mentorRequestSchema);
