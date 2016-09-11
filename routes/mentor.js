945a0f42e9c9dc3cd694448f6e8f866531901263
Trevor Edwards <tre23@cornell.edu>
2016-09-11 14:56:35 -0400

    delete old mentor functions

--- routes/mentor.js
+++ routes/mentor.js
@@ -32,72 +32,6 @@ module.exports = function (io) {
     });
 
     /**
-     * @api {POST} /mentor/updateinformation Update mentor's info
-     * @apiName UpdateInformation
-     * @apiGroup Mentor
-     */
-    router.post('/updateinformation', function (req, res) {
-        var user = req.user;
-
-        var splitSkills = req.body.skills.split(",");
-        var skillList = [];
-        for (var i = 0; i < splitSkills.length; i++) {
-            if (splitSkills[i].trim() != "") {
-                skillList.push(splitSkills[i].trim());
-            }
-        }
-        user.mentorinfo.skills = skillList;
-        user.mentorinfo.bio = req.body.bio;
-        user.save(function(err) {
-            if (err) {
-                console.error(err);
-                req.flash("error", "An error occurred. Try updating again in a bit.");
-            }
-            else {
-                //redirect to dashboard home
-                req.flash("success", "Information updated successfully.");
-            }
-            MentorRequest.find({}).exec(function (err, mentorRequests) {
-                User.find({role: "mentor"}).exec(function (err, mentors) {
-                    async.each(mentorRequests, function (mentorRequest, callback) {
-                        var numMatchingMentors = 0;
-                        async.each(mentors, function (mentor, callback2) {
-                            if (_matchingSkills(mentor.mentorinfo.skills, mentorRequest.skills)) {
-                                numMatchingMentors = numMatchingMentors + 1;
-                            }
-                            callback2();
-                        }, function (err) {
-                            if (err) console.error(err);
-                            else {
-                                mentorRequest.nummatchingmentors = numMatchingMentors;
-                                mentorRequest.save(function (err) {
-                                    if (err) console.error(err);
-                                    else {
-                                        User.findOne({_id: mentorRequest.user.id}, function (err, user) {
-                                            if (err) console.error(err);
-                                            else {
-                                                var currentMentorRequest = {
-                                                    mentorRequestPubid: mentorRequest.pubid,
-                                                    nummatchingmentors: numMatchingMentors
-                                                };
-                                                io.sockets.emit("new number of mentors " + user.pubid, currentMentorRequest);
-                                                callback();
-                                            }
-                                        });
-                                    }
-                                });
-                            }
-                        });
-                    }, function (err) {
-                        if (err) console.error(err);
-                        res.redirect('/mentor/dashboard');
-                    });
-                });
-            });
-        });
-    });
-
-    /**
      * @api {GET} /mentor/requestsqueue See requests queue of mentor.
      * @apiName RequestQueue
      * @apiGroup Mentor
@@ -185,41 +119,6 @@ module.exports = function (io) {
             });
         });
     });
-
-    /**
-     * Returns true if there is any intersection between a mentor's skills (mentorSkills) and the user's
-     * skills (userSkills), false otherwise
-     * @param mentorSkills string array representing mentor's skills
-     * @param userSkills string array representing user's skills
-     * @returns boolean whether or not there is an intersection between a mentor's skills and the user's skills
-     */
-    function _matchingSkills(mentorSkills, userSkills) {
-        for (var i = 0; i < mentorSkills.length; i++) {
-            for (var j = 0; j < userSkills.length; j++) {
-                //Check equality of first five characters so there is a match between skills like "mobile app dev"
-                //and "mobile applications"
-                if (mentorSkills[i].toLowerCase().substring(0, 5) == userSkills[j].toLowerCase().substring(0,5)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Returns the string representing the image url of a company (ex: img/logos/uber.png for Uber) given a company name
-     * @param mentorCompany string representing company of a mentor
-     * @returns string representing image url of company
-     */
-    function _getCompanyImage(mentorCompany) {
-        var companyNameList = enums.mentor.companyname;
-        var companyImageList = enums.mentor.companyimage;
-        for (var i = 0; i < companyNameList.length; i++) {
-            if (companyNameList[i] == mentorCompany) {
-                return "/img/logos/" + companyImageList[i];
-            }
-        }
-    }
 
     /**
      * @api {GET} /mentor/logout Logout the current mentor
