3d2b2ce595a9b3a86a53d0bec53723173883f19b
Trevor Edwards <tre23@cornell.edu>
2016-09-12 22:21:17 -0400

    less ES6

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -1010,7 +1010,8 @@ function setInventory(req, res, next) {
  * @apiParam {Number} quantity The quantity of hardware to transact
  * @apiParam {String} name The unique name of the hardware being transacted
  **/
-function transactHardware({body}, res, next) {
+function transactHardware(req, res, next) {
+    var body = req.body;
     if (body.checkingOut === undefined || !body.email || body.quantity === undefined || !body.name) {
         return res.status(500).send('Missing a parameter, check the API!');
     }
