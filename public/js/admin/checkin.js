4899ffc7e19a63e9235486148cef8fc8a4b5a522
Austin Liu <atrix315@gmail.com>
2016-11-22 18:57:38 -0500

    198 comment: Moving js to file

--- public/js/admin/checkin.js
+++ public/js/admin/checkin.js
@@ -6,6 +6,31 @@ app.controller('checkin.ctrl', ['$scope', '$http', function ($scope, $http) {
     $scope.users = [];
     $scope.inputSearch = "";
 
+    //For QR scanning
+    var qr = new QCodeDecoder;
+    var video = document.getElementById('camera');
+    if (!qr.isCanvasSupported() || !qr.hasGetUserMedia()){
+        throw alert("Your browser doesn't match the required specs."),
+        new Error("Canvas and getUserMedia are required");
+    }
+    var elems = [{
+        target: document.querySelector("#camera video"),
+        activator: document.querySelector("#camera button"),
+        decoder: qr.decodeFromCamera
+    }];
+    elems.forEach(function(e) {
+        e.activator.onclick = function(r) {
+            r && r.preventDefault(),
+            e.decoder.call(qr, e.target, function(e, r) {
+            if (e){
+                throw e;
+            }
+            alert("Just decoded: " + r);
+            location.reload();
+            }, true)
+        }   
+    });
+
     $scope.filterSearch = function (user) {
         var input = $scope.inputSearch.toLowerCase();
         var name = (user.name.first + " " + user.name.last).toLowerCase();
